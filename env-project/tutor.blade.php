6a486dd92d107f669407948b821cdf248254fdf2
virda-aulia <auliajaguarte@gmail.com>
2024-03-19 21:09:07 +0700

    Add: Create Env From Project

---
+++ env-project/tutor.blade.php
@@ -0,0 +1,8 @@
+=========================
+# CARA MEMBUAT ENV #
+=========================
+
+1. Rename .env.example jadi .env
+2. Copy in konfigurasi .env pada folder env-project ke .env yang baru dibuat pada cara ke - 1
+3. Sesuaikan nama database, user database, password database (jika ada)
+4. Selesai
