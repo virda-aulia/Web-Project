218db26fd541220b72800219a1971cc74c1991c9
virda-aulia <auliajaguarte@gmail.com>
2024-03-19 21:30:40 +0700

    Add: Create File Component JS and CSS

---
+++ resources/views/pages/layouts/css/file-css.blade.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/fontawesome.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/tabler-icons.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/flag-icons.css" />
+
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/css/demo.css" />
+
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/node-waves/node-waves.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/typeahead-js/typeahead.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/apex-charts/apex-charts.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/swiper/swiper.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
+
+<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/pages/cards-advance.css" />
+
+<script src="{{ url('web-theme') }}/assets/vendor/js/helpers.js"></script>
+<script src="{{ url('web-theme') }}/assets/vendor/js/template-customizer.js"></script>
+<script src="{{ url('web-theme') }}/assets/js/config.js"></script>
