14e8fa899cddcbe32a474be69a9ddd8262729f2f
virda-aulia <auliajaguarte@gmail.com>
2024-03-19 21:29:17 +0700

    Create Routing Templating

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/templating", [AppController::class, "templating"]);
