ff5cde2e051cd7620b1a8ea5519918c071dd3214
virda-aulia <auliajaguarte@gmail.com>
2024-03-21 21:18:31 +0700

    Add: Create Routing

---
+++ app/Http/Controllers/Authorization/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Authorization;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    protected $users;
+
+    public function __construct()
+    {
+        $this->users = new User();
+    }
+
+    public function login()
+    {
+        try {
+            return view("pages.authorization.login");
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
