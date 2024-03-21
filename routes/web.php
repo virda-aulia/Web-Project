0ca06ae72f1fe98ca2cf3d01d67db41877f4c6c0
virda-aulia <auliajaguarte@gmail.com>
2024-03-21 21:17:59 +0700

    Add: Create Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\Authorization\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get("/layouts-template", [AppController::class, "templating"]);
+
+Route::controller(LoginController::class)->group(function() {
+    Route::get("/login", "login");
+    Route::post("/login", "proses_login");
+});
