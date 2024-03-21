e543b7ffbc1598f8ccd10b807cfa29a86a02aa51
virda-aulia <auliajaguarte@gmail.com>
2024-03-21 20:56:36 +0700

    Modified: Update Content in file.css.blade.php

--- resources/views/pages/layouts/css/file-css.blade.php
+++ resources/views/pages/layouts/css/file-css.blade.php
@@ -1,22 +1,32 @@
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/fontawesome.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/tabler-icons.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/fonts/flag-icons.css" />
+<link rel="icon" type="image/x-icon" href="{{ asset('template') }}/assets/img/favicon/favicon.ico" />
 
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/css/demo.css" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+    rel="stylesheet" />
 
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/node-waves/node-waves.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/typeahead-js/typeahead.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/apex-charts/apex-charts.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/swiper/swiper.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
+<link rel="stylesheet" href="{{ asset('template/vendor/fonts/fontawesome.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/fonts/tabler-icons.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/fonts/flag-icons.css') }}" />
 
-<link rel="stylesheet" href="{{ url('web-theme') }}/assets/vendor/css/pages/cards-advance.css" />
+<link rel="stylesheet" href="{{ asset('template/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
+<link rel="stylesheet" href="{{ asset('template/vendor/css/rtl/theme-default.css') }}"
+    class="template-customizer-theme-css" />
+<link rel="stylesheet" href="{{ asset('template/css/demo.css') }}" />
 
-<script src="{{ url('web-theme') }}/assets/vendor/js/helpers.js"></script>
-<script src="{{ url('web-theme') }}/assets/vendor/js/template-customizer.js"></script>
-<script src="{{ url('web-theme') }}/assets/js/config.js"></script>
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/node-waves/node-waves.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/typeahead-js/typeahead.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/apex-charts/apex-charts.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/swiper/swiper.css') }}" />
+<link rel="stylesheet" href="{{ asset('template/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}" />
+<link rel="stylesheet"
+    href="{{ asset('template/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
+<link rel="stylesheet"
+    href="{{ asset('template/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('template/vendor/css/pages/cards-advance.css') }}" />
+<script src="{{ asset('template/vendor/js/helpers.js') }}"></script>
+<script src="{{ asset('template/vendor/js/template-customizer.js') }}/"></script>
+<script src="{{ asset('template/js/config.js') }}"></script>
